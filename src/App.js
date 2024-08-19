1626b35d6f9a1c470335394230fdd2139fae6c48
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-08-18 23:55:28 -0300

    feat: create task component and tasks list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  ); 
-}  
+    <>
+      <section className="container">
+          <Tasks />
+      </section>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tasks = () => {
+  return (
+    <article>
+      <h1>Tasks</h1>
+    </article>
+  );
+};
+
+export default Tasks;
